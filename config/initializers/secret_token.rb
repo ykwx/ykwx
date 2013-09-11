91b27fc92c1de1a35f1ad03b1feac0c3fb6117a1
Jonathan Fagan <Jonathan@Jonathans-MacBook-Pro.local>
2013-09-11 19:08:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Psychapp::Application.config.secret_key_base = '8a6bd33fa9174a9aa464b16dfdfa3d3651ab41c8a345a944d1d8acd762d18ddf98b8fecbb9873d366d3dd160ed51eddd6a3ecacfe2e3342350df72c82210f5d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Psychapp::Application.config.session_store :cookie_store, key: '_psychapp_session'
