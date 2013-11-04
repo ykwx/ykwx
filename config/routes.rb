5e71870e25898b31d7876de23b27e7925f6b8447
Jonathan Fagan <jonathan@proteinos.com>
2013-11-04 19:02:34 +0000

    Added in other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Psychapp::Application.routes.draw do
 
-  get "welcome/index"
-  get "/berlin", to: "shared#berlin"
+  get "static_pages/index"
 
-  root 'welcome#index'
+  get "/barcelona", to: "static_pages#barcelona"
+  get "/croatia", to: "static_pages#croatia"
+  get "/trieste", to: "static_pages#trieste"
+  get "/berlin", to: "static_pages#berlin"
+  get "/majorca", to: "static_pages#majorca"
+
+  root 'static_pages#index'
 
 end
